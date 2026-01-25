55a2367ea786541d4028185bc206fefe9531494e
svr-1101 <148403990+svr-1101@users.noreply.github.com>
2026-01-25 19:22:10 +0700

    Delete carsharing directory

--- carsharing/footer.php
+++
@@ -1,6 +0,0 @@
-</div> <!-- Конец container -->
-    <footer style="background: #333; color: #fff; text-align: center; padding: 20px; margin-top: 40px;">
-        <p>&copy; 2024 Курсовая работа "Каршеринг"</p>
-    </footer>
-</body>
-</html>
--- carsharing/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Удаляем сессию
-header("Location: index.php"); // Возвращаем на главную
-?>
